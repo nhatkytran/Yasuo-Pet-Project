3368f3091060e49d2fa7059af4931cb4b3a6d3a5
nhatkytran <nhatky.tran.2002@gmail.com>
2023-04-22 12:34:50 +0700

    Frontend - Fetch Video Trailer

--- FrontEnd/src/js/controllers/mainController.js
+++ FrontEnd/src/js/controllers/mainController.js
@@ -1,3 +1,38 @@
-const fetchVideoButton = document.querySelector('.trailer__play-video-play');
+import axios from 'axios';
 
-fetchVideoButton.addEventListener('click', async () => {});
+const fetchVideoButton = document.querySelector('.trailer__play-video-play');
+const fetchVideoLoading = document.querySelector(
+  '.trailer__play-video-loading'
+);
+const fetchVideoSuccess = document.querySelector(
+  '.trailer__play-video-success'
+);
+const fetchVideoMessage = document.querySelector(
+  '.trailer__play-video-message'
+);
+const fetchVideoAgain = fetchVideoMessage.querySelector('span');
+
+const fetchVideo = async () => {
+  try {
+    fetchVideoMessage.classList.add('remove');
+    fetchVideoButton.classList.add('remove');
+    fetchVideoLoading.classList.remove('remove');
+
+    const { data } = await axios({
+      method: 'GET',
+      url: 'http://127.0.0.1:3000/api/v1/subweb/video',
+    });
+
+    fetchVideoLoading.classList.add('remove');
+    fetchVideoSuccess.classList.remove('remove');
+
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+    fetchVideoLoading.classList.add('remove');
+    fetchVideoMessage.classList.remove('remove');
+  }
+};
+
+fetchVideoButton.addEventListener('click', fetchVideo);
+fetchVideoAgain.addEventListener('click', fetchVideo);
