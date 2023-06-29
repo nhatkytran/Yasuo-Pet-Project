c372cde9676fd1035912afd36dfdea31cb66e985
nhatkytran <nhatky.tran.2002@gmail.com>
2023-06-29 10:29:16 +0700

    Frontend - Abilities Fix description; Chose Skill; Dev Data

---
+++ FrontEnd/src/dev-data/js2/controllers/mainController.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import { $, $$ } from '../config';
+
+const skillsContainer = $('.ab__skills');
+const skills = $$('.ab__skills-skill');
+const skillCircle = $('.ab__skills-progress-circle');
+
+skills.forEach((skill, index) => skill.setAttribute('data-ab-skill', index));
+
+const skillWidth = 9.6; // (rem) 96px for each skill (only displayed on PC)
+const skillWidthAdder = 4.1;
+
+let lastSkillChosen;
+
+skillsContainer.addEventListener('click', event => {
+  const target = event.target.closest('.ab__skills-skill');
+
+  if (!target) return;
+
+  const index = Number(target.dataset.abSkill);
+
+  handleSkillChosen(index);
+});
+
+function handleSkillChosen(index) {
+  skillCircle.style.left = `${skillWidth * index + skillWidthAdder}rem`;
+
+  if (lastSkillChosen !== undefined)
+    skills[lastSkillChosen].classList.remove('active');
+
+  lastSkillChosen = index;
+  skills[index].classList.add('active');
+}
+
+function handleDescription(index) {}
+
+function handleVideo(index) {}
