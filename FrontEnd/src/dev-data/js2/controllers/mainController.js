e85081a3b6f53ddb2a806fc8373da1b03c70b624
nhatkytran <nhatky.tran.2002@gmail.com>
2023-07-18 11:38:17 +0700

    Frontend - Skins2 Basic Drag Slider

--- FrontEnd/src/dev-data/js2/controllers/mainController.js
+++ FrontEnd/src/dev-data/js2/controllers/mainController.js
@@ -3,6 +3,7 @@
 import { $, $$ } from '../../../js/utils';
 
 const slider = $('.skins2-slider');
+const sliderList = $('.skins2-slider-list');
 const slideItems = $$('.skins2-slider-item');
 const slideButtons = $$('.skins2-button');
 
@@ -10,32 +11,87 @@ slideButtons.forEach((button, index) => {
   button.setAttribute('data-slide-button-index', index);
 });
 
+let isDragged = false; // if true --> prevent `click` event
+let isReadyToDrag = false;
+let oldClientY;
+let newClientY;
+
+function dragStart(event) {
+  isReadyToDrag = true;
+  oldClientY = event.clientY;
+}
+
+function dragProgress(event) {
+  if (!isReadyToDrag) return;
+  console.log(currentPercent);
+
+  isDragged = true;
+  newClientY = event.clientY;
+
+  const diff = newClientY - oldClientY;
+
+  if (diff === 0) return;
+
+  // dif > 0 and diff < 0
+  slideItems.forEach(slideItem => {
+    slideItem.style.transform = `translateY(${currentPercent + diff}%)`;
+  });
+}
+
+function dragStop(event) {
+  isReadyToDrag = false;
+
+  // `mouseup` <--> `click`
+  // Handle isDragged with `mousedown` (`click` set isDragged to false by itself)
+  if (event.type === 'mouseleave') isDragged = false;
+
+  // Adjust currentPercent and Position
+  currentPercent += newClientY - oldClientY;
+
+  if (currentPercent > 100) currentPercent = 100;
+
+  // <
+
+  slideItems.forEach(slideItem => {
+    slideItem.style.transform = `translateY(${currentPercent}%)`;
+  });
+}
+
+sliderList.addEventListener('mousedown', dragStart);
+sliderList.addEventListener('mousemove', dragProgress);
+sliderList.addEventListener('mouseup', dragStop);
+sliderList.addEventListener('mouseleave', dragStop);
+
 let prevIndex = 1; // By default, index of 1 is active
+let currentIndex = 1;
+let currentPercent = (currentIndex - 1) * -100;
 
 slider.addEventListener('click', event => {
+  if (isDragged) return (isDragged = false);
+
   const target = event.target.closest('.skins2-button');
 
   if (!target) return;
 
   const index = Number(target.dataset.slideButtonIndex);
 
-  slide(index);
-  slideAnimate(prevIndex, index);
+  currentIndex = index;
+
+  slide();
+  slideAnimate();
 
   prevIndex = index;
-
-  console.log(index);
 });
 
-function slide(currentIndex) {
-  const shouldIndex = (currentIndex - 1) * -100;
+function slide() {
+  currentPercent = (currentIndex - 1) * -100;
 
   slideItems.forEach(slideItem => {
-    slideItem.style.transform = `translateY(${shouldIndex}%)`;
+    slideItem.style.transform = `translateY(${currentPercent}%)`;
   });
 }
 
-function slideAnimate(prevIndex, currentIndex) {
+function slideAnimate() {
   if (prevIndex !== null) slideButtons[prevIndex].classList.remove('active');
   slideButtons[currentIndex].classList.add('active');
 }
