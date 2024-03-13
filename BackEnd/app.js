1c28b8c8769e89662e0f8af118b0fe83ac6373db
nhatkytran <nhatky.tran.2002@gmail.com>
2024-03-13 17:27:14 +0700

    Backend - Cors origin only 1

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -80,7 +80,10 @@ app.enable('trust proxy');
 // Cors
 app.use(
   cors({
-    origin: ['http://127.0.0.1:8080', 'https://yasuo-front.netlify.app/'],
+    origin:
+      NODE_ENV === 'development'
+        ? 'http://127.0.0.1:8080'
+        : 'https://yasuo-front.netlify.app/',
     credentials: true,
   })
 );
