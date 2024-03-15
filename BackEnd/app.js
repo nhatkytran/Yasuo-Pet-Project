85e5dedd159597cf927ba3970e2924801b371b4c
nhatkytran <nhatky.tran.2002@gmail.com>
2024-03-15 12:18:39 +0700

    Backend - Ad more cors url undefined

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -84,7 +84,8 @@ let corsOrigin;
 if (NODE_ENV === 'development') corsOrigin = 'http://127.0.0.1:8080';
 else {
   const whilelist = [
-    'https://yasuo-the-king.onrender.com',
+    undefined, // 'https://yasuo-the-king.onrender.com'
+    // google
     'https://yasuo-the-king.netlify.app',
     'https://dashboard.stripe.com',
   ];
